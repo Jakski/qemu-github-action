c0cda260d68e036c5e2ad941d2844a3899cf347e
Jakub Pieńkowski <8525083+Jakski@users.noreply.github.com>
2024-06-04 23:30:04 +0200

    Update APT cache before installing dependencies

--- main.sh
+++ main.sh
@@ -97,7 +97,7 @@ install_dependencies() {
 			sudo apt-get update -y
 			sudo apt-get install -y "${pkgs[@]}"
 		else
-			apt-get update
+			apt-get update -y
 			apt-get install -y "${pkgs[@]}"
 		fi
 	fi
